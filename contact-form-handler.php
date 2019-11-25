0f203443ade82e3ef1ce9d4f78cf5c5182c8161a
Artificiels <52905106+Artificiels@users.noreply.github.com>
2019-11-25 00:24:48 -0500

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST ['email'];
+$message = $_POST ['message'];
+
+$email_form = 'EasyTutorials@avinashkr.com';
+$email_subject = "New Form Submission";
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n";
+
+$to = "contact@groupeeconovert.ca";
+$headers = "From: $email_form \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: index.html");
+
+ ?>
